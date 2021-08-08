714b6ca0400d2aabf9e19050779a72386f7888f7
Izuru Kambayashi <ki.yr.0727@gmail.com>
2021-08-08 18:07:02 +0900

    iOS firebase setup

--- lib/components/QandA/QandA.dart
+++ lib/components/QandA/QandA.dart
@@ -1,15 +1,66 @@
+import 'package:provider/provider.dart';
+
 import '../../importer.dart';
+
+// class QandA extends StatelessWidget {
+//   QandA();
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('よくある質問'),
+//       ),
+//       body: Container(),
+//     );
+//   }
+// }
+
+class CountModel extends ChangeNotifier {
+  int count = 0;
+  void increment() {
+    count++;
+    notifyListeners();
+  }
+}
 
 class QandA extends StatelessWidget {
   QandA();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('よくある質問'),
+    return ChangeNotifierProvider<CountModel>(
+      create: (context) => CountModel(),
+      child: Consumer<CountModel>(
+        builder: (context, model, child) => Scaffold(
+          appBar: AppBar(
+            title: Text('title'),
+          ),
+          body: Center(
+            child: Column(
+              children: [
+                Text('おしましたね？'),
+                CountText(),
+              ],
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: model.increment,
+            tooltip: 'Increment',
+            child: Icon(Icons.add),
+          ),
+        ),
       ),
-      body: Container(),
+    );
+  }
+}
+
+class CountText extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      '${Provider.of<CountModel>(context).count}',
+      style: Theme.of(context).textTheme.headline4,
     );
   }
 }
